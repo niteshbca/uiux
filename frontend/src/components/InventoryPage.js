cc2ab446083b063dde376f446d5c0dd88e10cd7c
Nitesh Kumar Singh <138958926+niteshbca@users.noreply.github.com>
2025-01-31 21:21:56 +0530

    Update InventoryPage.js

--- frontend/src/components/InventoryPage.js
+++ frontend/src/components/InventoryPage.js
@@ -17,7 +17,7 @@ const InventoryPage = () => {
       }
 
       try {
-        const response = await axios.get("http://localhost:5000/api/getDeliveryItems", {
+        const response = await axios.get("http://56.124.27.238:5000/api/getDeliveryItems", {
           params: { godown: godown.name }, // Pass godown name as a query parameter
         });
 
